df262bff4a50175b73c96a60463cbc0434907f7c
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-08-13 12:18:34 -0300

    Add deleteCart/GetCarts and error handling with try/catch

--- src/controllers/cartManager.js
+++ src/controllers/cartManager.js
@@ -8,47 +8,83 @@ export default class CartManager {
     }
 
     async createCart() {
-        const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'))
-        let newID = 1;
+        try {
+            const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'))
+            let newID = 1;
 
-        if (carts.length > 0) {
-            newID = carts[carts.length - 1].id + 1
+            if (carts.length > 0) {
+                newID = carts[carts.length - 1].id + 1
+            }
+
+            carts.push({
+                id: newID,
+                products: []
+            })
+            await fs.writeFile(this.cartsPath, JSON.stringify(carts))
+            return true
+        } catch (error) {
         }
-
-        carts.push({
-            id: newID,
-            products: []
-        })
-        await fs.writeFile(this.cartsPath, JSON.stringify(carts))
-        return true
     }
 
     async getCart(id) {
-        const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'))
-        const cart = carts.find(cart => cart.id === parseInt(id))
-        return cart
+        try {
+            const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'))
+            const cart = carts.find(cart => cart.id === parseInt(id))
+            return cart
+        } catch (error) {
+            console.log(error)
+        }
     }
 
+    async getCarts() {
+        try {
+            const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'))
+            return carts
+        } catch (error) {
+            console.log(error)
+        }
+    }
     async addToCart(cartID, productID) {
-        const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'));
-        const products = JSON.parse(await fs.readFile(this.productsPath, 'utf-8'));
+        try {
+            const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'));
+            const products = JSON.parse(await fs.readFile(this.productsPath, 'utf-8'));
 
-        const cart = carts.find(cart => cart.id === parseInt(cartID));
-        const producto = products.find(product => product.id === parseInt(productID));
+            const cart = carts.find(cart => cart.id === parseInt(cartID));
+            const producto = products.find(product => product.id === parseInt(productID));
 
-        if (cart && producto) {
-            const existingProduct = cart.products.find(prod => prod.product === producto.id);
+            if (cart && producto) {
+                const existingProduct = cart.products.find(prod => prod.product === producto.id);
 
-            if (existingProduct) {
-                existingProduct.quantity++;
-            } else {
-                cart.products.push({
-                    product: producto.id,
-                    quantity: 1
-                });
+                if (existingProduct) {
+                    existingProduct.quantity++;
+                } else {
+                    cart.products.push({
+                        product: producto.id,
+                        quantity: 1
+                    });
+                }
+                await fs.writeFile(this.cartsPath, JSON.stringify(carts));
+                return true;
             }
-            await fs.writeFile(this.cartsPath, JSON.stringify(carts));
-            return true;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async deleteCart(cartID) {
+        try {
+            const carts = JSON.parse(await fs.readFile(this.cartsPath, 'utf-8'));
+            const cart = carts.find(cart => cart.id === parseInt(cartID));
+
+            if (cart) {
+                const newCart = carts.filter(cart => cart.id !== parseInt(cartID));
+                await fs.writeFile(this.cartsPath, JSON.stringify(newCart))
+                console.log("Carrito eliminado correctamente")
+                return true
+            } else {
+                console.log("El carrito que intentas eliminar no existe")
+            }
+        } catch (error) {
+            console.log(error);
         }
     }
 }
--- src/controllers/productManager.js
+++ src/controllers/productManager.js
@@ -3,101 +3,114 @@ export default class ProductManager {
     constructor(path) {
         this.products = []
         this.path = path
-        this.IDgenerator = this.incrementalID()
-        this.lastID = 0
-    }
-
-    incrementalID = () => {
-        let id = 0
-        return () => {
-            id++
-            this.lastID = id
-            return id
-        }
     }
 
     async addProduct(title, description, price, thumbnail, code, stock, status, category) {
-        const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        try {
+            const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
 
-        if(typeof status !== "boolean") {
-            console.log("El status debe ser un booleano")
-            return false
-        } else {
-            status = status
-        }        
+            if (typeof status !== "boolean") {
+                status = true
+            } else {
+                status = status
+            }
 
-        if (products.find(product => product.code == code)) {
-            console.log('Ya existe un producto cargado con el codigo', code)
-        } else if (!title || !description || !price || !stock || !category) {
-            console.log("Asegurate de haber cargado todos los datos")
-        } else {
-            products.push({
-                id: this.IDgenerator(),
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: [thumbnail],
-                code: code,
-                stock: stock,
-                status: status,
-                category: category
-            })
-            await fs.writeFile(this.path, JSON.stringify(products))
-            console.log("Producto cargado correctamente con el id:", this.lastID)
-            return true
+            if (products.find(product => product.code == code)) {
+                console.log('Ya existe un producto cargado con el codigo', code)
+            } else if (!title || !description || !price || !stock || !category) {
+                console.log("Asegurate de haber cargado todos los datos")
+            } else {
+                let newID = 1;
+
+                if (products.length > 0) {
+                    newID = products[products.length - 1].id + 1
+                }
+                products.push({
+                    id: newID,
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: [thumbnail],
+                    code: code,
+                    stock: stock,
+                    status: status,
+                    category: category
+                })
+                await fs.writeFile(this.path, JSON.stringify(products))
+                console.log("Producto cargado correctamente con el id:", this.lastID)
+                return true
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 
     async getProducts() {
-        const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
-        return products
+        try {
+            const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
+            return products
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     async getProductById(id) {
-        const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
-        const product = products.find(prod => prod.id === id)
-        return product
+        try {
+            const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
+            const product = products.find(prod => prod.id === id)
+            return product
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     async updateProduct(id, { title, description, price, thumbnail, code, stock, status, category }) {
-        const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        try {
+            const products = JSON.parse(await fs.readFile(this.path, "utf-8"))
 
-        const index = products.findIndex(prod => prod.id === id)
-        if (index === -1) {
-            console.log("No existe un producto con la ID solicitada.")
-        } else if (!title || !description || !price || !thumbnail || !stock || !status || !category) {
-            console.log("Asegurate de haber cargado todos los datos")
-        }
-        else {
-            //Update the product here            
-            products[index] = {
-                id: id,
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: thumbnail,
-                code: code,
-                stock: stock,
-                status: status,
-                category: category
+            const index = products.findIndex(prod => prod.id === id)
+            if (index === -1) {
+                console.log("No existe un producto con la ID solicitada.")
+            } else if (!title || !description || !price || !thumbnail || !stock || !status || !category) {
+                console.log("Asegurate de haber cargado todos los datos")
             }
-            await fs.writeFile(this.path, JSON.stringify(products))
-            console.log("Producto actualizado correctamente con el id:", id)
-            return true
+            else {
+                //Update the product here            
+                products[index] = {
+                    id: id,
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: thumbnail,
+                    code: code,
+                    stock: stock,
+                    status: status,
+                    category: category
+                }
+                await fs.writeFile(this.path, JSON.stringify(products))
+                console.log("Producto actualizado correctamente con el id:", id)
+                return true
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
     async deleteProduct(id) {
-        const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
+        try {
+            const products = JSON.parse(await fs.readFile(this.path, 'utf-8'))
 
-        if (products.find(prod => prod.id == id)) {
-            //Get all the products except the one with the id
-            const newProducts = products.filter(product => product.id != id)
-            await fs.writeFile(this.path, JSON.stringify(newProducts))
-            console.log("Producto eliminado correctamente con el id:", id)
-            return true
-        }
-        else {
-            console.log("No existe un producto con la ID solicitada.")
+            if (products.find(prod => prod.id == id)) {
+                //Get all the products except the one with the id
+                const newProducts = products.filter(product => product.id != id)
+                await fs.writeFile(this.path, JSON.stringify(newProducts))
+                console.log("Producto eliminado correctamente con el id:", id)
+                return true
+            }
+            else {
+                console.log("No existe un producto con la ID solicitada.")
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 }
