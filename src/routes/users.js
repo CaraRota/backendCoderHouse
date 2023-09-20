804716777c7b870bffdbf2b4b38592c7376d4b99
CaraRota <canalsdigital@gmail.com>
2023-09-20 14:08:00 -0300

    Remove commented code on routes/users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -22,22 +22,4 @@ routerUser.post('/', async (req, res) => {
     }
 });
 
-// routerUser.post('/', async (req, res) => {
-//     const { first_name, last_name, email, password, age } = req.body;
-
-//     try {
-//         const response = await userModel.create({
-//             first_name,
-//             last_name,
-//             email,
-//             password,
-//             age
-//         })
-//         res.status(200).send({ mensaje: 'Usuario creado', respuesta: response })
-//     }
-//     catch (error) {
-//         res.status(400).send({ error: `Error al crear usuario: ${error}` });
-//     }
-// });
-
 export default routerUser;
