4302bcdefbaeedae7d2d7db789b4a00bd4576bdd
CaraRota <canalsdigital@gmail.com>
2023-11-11 16:32:05 -0300

    Fix mockingProducts so it gives 100 different products every time i hit the endpoint

--- src/routes/mockingProducts.js
+++ src/routes/mockingProducts.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { products } from "../utils/mockingProducts.js";
+import generateRandomProducts from "../utils/mockingProducts.js";
 import { passportError, authorization } from "../utils/messageErrors.js";
 
 const routerMockingProducts = Router();
 
 routerMockingProducts.get("/", passportError('jwt'), authorization(['admin']), (req, res) => {
+    const products = generateRandomProducts(100);
     res.json(products);
 });
 
--- src/utils/mockingProducts.js
+++ src/utils/mockingProducts.js
@@ -1,20 +1,25 @@
 import { faker } from "@faker-js/faker";
 
-const generateProducts = (quantity) => {
+
+
+const generateProducts = () => {
+    return {
+        title: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        price: faker.commerce.price(),
+        stock: faker.number.int(100),
+        code: faker.string.uuid(),
+        category: faker.commerce.department(),
+        status: faker.datatype.boolean(),
+    }
+}
+
+const generateRandomProducts = (quantity) => {
     const products = [];
     for (let i = 0; i < quantity; i++) {
-        const product = {
-            title: faker.commerce.productName(),
-            description: faker.commerce.productDescription(),
-            price: faker.commerce.price(),
-            stock: faker.number.int(100),
-            code: faker.string.uuid(),
-            category: faker.commerce.department(),
-            status: faker.datatype.boolean(),
-        };
-        products.push(product);
+        products.push(generateProducts());
     }
     return products;
-};
+}
 
-export const products = generateProducts(100);
+export default generateRandomProducts;
