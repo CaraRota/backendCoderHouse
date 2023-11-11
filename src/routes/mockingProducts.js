10f9a701e65b62649db660380377d52165a4a915
CaraRota <canalsdigital@gmail.com>
2023-11-11 16:35:07 -0300

    Add http logger in /mockingProducts

--- src/routes/mockingProducts.js
+++ src/routes/mockingProducts.js
@@ -2,9 +2,13 @@ import { Router } from "express";
 import generateRandomProducts from "../utils/mockingProducts.js";
 import { passportError, authorization } from "../utils/messageErrors.js";
 
+//IMPORT LOGGERS
+import loggers from '../utils/loggers.js'
+
 const routerMockingProducts = Router();
 
 routerMockingProducts.get("/", passportError('jwt'), authorization(['admin']), (req, res) => {
+    loggers.http('GET /mockingProducts');
     const products = generateRandomProducts(100);
     res.json(products);
 });
