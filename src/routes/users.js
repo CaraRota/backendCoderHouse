028bd0a980a2350d33c3ee48c14bf27d10fa9379
CaraRota <canalsdigital@gmail.com>
2023-12-19 11:50:35 -0300

    Add documents upload and last_connection to the users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import passport from "passport";
-import { passwordRecovery, registerUser, resetPassword, deleteUser, getUsers } from "../controllers/users.js";
+import { authorization } from "../utils/messageErrors.js";
+import { passwordRecovery, registerUser, resetPassword, deleteUser, getUsers, uploadDocuments } from "../controllers/users.js";
+import { upload } from "../config/config.js";
 
 const routerUser = Router();
 
@@ -8,6 +10,7 @@ routerUser.get('/', passport.authenticate('jwt', { session: false }), getUsers);
 routerUser.post('/', passport.authenticate('register'), registerUser);
 routerUser.post('/password-recovery', passwordRecovery);
 routerUser.post('/reset-password/:token', resetPassword);
+routerUser.post('/:id/documents', passport.authenticate('jwt', { session: false }), authorization(['user']), upload.fields([{ name: 'documents' }]), uploadDocuments);
 
 //Added for testing purposes
 routerUser.delete('/:id', passport.authenticate('jwt', { session: false }), deleteUser);
