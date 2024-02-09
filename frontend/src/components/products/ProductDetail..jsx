b0f9e0d1c0d8903ad353d287ac518629487d80e9
CaraRota <canalsdigital@gmail.com>
2024-02-09 10:35:07 -0300

    Refactor ProductDetail component to use react-router-dom for login redirection

--- frontend/src/components/products/ProductDetail..jsx
+++ frontend/src/components/products/ProductDetail..jsx
@@ -1,42 +1,57 @@
-import React from 'react'
+import React from "react";
 
 //Assets
-import noImage from '../../assets/noproductimage.png'
+import noImage from "../../assets/noproductimage.png";
 
 //MUI
-import { Typography, Card, CardContent, CardMedia, Button } from '@mui/material'
-import ProductCount from './ProductCount'
+import { Typography, Card, CardContent, CardMedia, Button } from "@mui/material";
+import ProductCount from "./ProductCount";
+
+//RRD
+import { Link } from "react-router-dom";
 
 //User Context
-import { useUser } from '../../hooks/UserContext'
+import { useUser } from "../../hooks/UserContext";
 
 const ProductDetail = ({ product }) => {
-    const { loggedIn } = useUser()
+    const { loggedIn } = useUser();
 
     return (
-        <Card style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Card style={{ height: "100%", display: "flex", flexDirection: "column" }}>
             <CardMedia
-                style={{ paddingTop: '56.25%' }} // 16:9
+                style={{ paddingTop: "56.25%" }} // 16:9
                 image={product.thumbnail[0] === "none" ? noImage : product.thumbnail[0]}
                 title={product.title}
             />
             <CardContent style={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h5" component="div">
+                <Typography gutterBottom variant='h5' component='div'>
                     {product.title}
                 </Typography>
                 <Typography paragraph>{product.description}</Typography>
-                <Typography variant="h6" color="primary" gutterBottom>
+                <Typography variant='h6' color='primary' gutterBottom>
                     {product.price} USD
                 </Typography>
             </CardContent>
-            {
-                loggedIn ?
-                    <ProductCount initialCount={0} stock={product.stock} productID={product._id} />
-                    : <Button variant="outlined" color="primary" href="/login" sx={{ width: "95%", alignContent: "center", display: "block", margin: "auto", marginBottom: "1.2rem" }}>Inicia sesión para comprar</Button>
-            }
-
+            {loggedIn ? (
+                <ProductCount initialCount={0} stock={product.stock} productID={product._id} />
+            ) : (
+                <Link to={"/login"}>
+                    <Button
+                        variant='outlined'
+                        color='primary'
+                        sx={{
+                            width: "95%",
+                            alignContent: "center",
+                            display: "block",
+                            margin: "auto",
+                            marginBottom: "1.2rem",
+                        }}>
+                        Inicia sesión para comprar
+                    </Button>
+                </Link>
+            )}
         </Card>
-    )
-}
+    );
+};
 
-export default ProductDetail
+export default ProductDetail;
